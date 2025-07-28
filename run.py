a86c436577b6242950069bc50913c0e876204c53
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2025-07-28 04:19:05 -0500

    Run gunicorn in run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import subprocess
 from __init__ import create_app
 
 config = {}
@@ -6,4 +8,12 @@ config = {}
 if os.environ.get('FLASK_DEBUG') == '1':
 	config['DEBUG'] = True
 
-app = create_app(config) 
+app = create_app(config)
+
+if __name__ == '__main__':
+	gunicorn_args = ['gunicorn', '-c', 'gunicorn.conf.py', 'run:app']
+	
+	if os.environ.get('FLASK_DEBUG') == '1':
+		gunicorn_args.append('--reload')
+	
+	subprocess.run(gunicorn_args) 
